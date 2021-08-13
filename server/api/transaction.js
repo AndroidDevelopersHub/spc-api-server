589bb6f39161a1c2fb37253d2a94542f944659b6
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-08-13 15:49:00 +0600

    Update API

--- server/api/transaction.js
+++ server/api/transaction.js
@@ -16,8 +16,29 @@ const commonServe = require('../common/services/commonServices')
 
 module.exports = function (router) {
     router.put('/transaction', update);
+    router.get('/transaction/:uid', listById);
+
 }
 
+
+
+
+
+function listById(req ,res ){
+    //const result = bcrypt.compareSync('123', hash);
+    if (req.params.uid){
+        db.query("SELECT * FROM `balance_convert_history` WHERE uid='"+req.params.uid+"' OR uid="+req.params.uid+"", (err, result) => {
+            console.log(result)
+            if (!err && result.length > 0) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.found ,result)
+            } else {
+                return _response.apiWarning(res , responsemsg.listIsEmpty)
+            }
+        });
+    }else {
+        return _response.apiWarning(res , 'Please select id')
+    }
+}
 
 
 function update(req ,res ){
@@ -34,31 +55,49 @@ function update(req ,res ){
 
                 if (parseInt(req.body.amount) <= parseInt(win_cash)){
 
-                    const result = ( (102/100) * parseInt(req.body.amount)).toFixed(2)
-                    const result2 = ( (2/100) * parseInt(req.body.amount)).toFixed(2)
-
-                    if (result <= win_cash){
-                        let finalWinCash  = win_cash - result;
-                        let finalRaw_cash = raw_cash + parseInt(req.body.amount);
-                        db.query("UPDATE users SET ? WHERE uid = '"+req.body.uid+"'" , {win_cash: finalWinCash , raw_cash: finalRaw_cash} ,(err , result1) =>{
-                            if (!err){
-
-                                responseData.charge = result2;
-                                responseData.transferred_amount = req.body.amount
-                                responseData.win_cash_decrement_amount = result;
-                                responseData.optional = result1
-
-                                return _response.apiSuccess(res, "Balance Transferred Successfully" ,responseData)
-                            }else{
-                                return _response.apiFailed(res, err)
-                            }
-                        })
+                    if (parseInt(req.body.amount) < 50){
+                        return  _response.apiWarning(res,"Minimum balance required 50TK")
+                    }else{
 
 
+                        const result = ( (102/100) * parseInt(req.body.amount))
+                        const result2 = ( (2/100) * parseInt(req.body.amount))
 
-                    }else {
-                        return  _response.apiWarning(res,"Low balance!")
+                        if (result <= win_cash){
+                            let finalWinCash  = win_cash - result;
+                            let finalRaw_cash = raw_cash + parseInt(req.body.amount);
+                            db.query("UPDATE users SET ? WHERE uid = '"+req.body.uid+"'" , {win_cash: finalWinCash , raw_cash: finalRaw_cash} ,(err , result1) =>{
+                                if (!err){
+
+                                    responseData.charge = result2;
+                                    responseData.transferred_amount = parseInt(req.body.amount)
+                                    responseData.win_cash_decrement_amount = result;
+                                    responseData.optional = result1
+
+                                    db.query("INSERT INTO `balance_convert_history` SET ?", {
+                                        uid: req.body.uid,
+                                        amount: req.body.amount,
+                                        charge: responseData.charge,
+                                        total: result
+
+                                    }, (err, result) => {
+                                    });
+
+
+                                    return _response.apiSuccess(res, "Balance Transferred Successfully" ,responseData)
+                                }else{
+                                    return _response.apiFailed(res, err)
+                                }
+                            })
+
+
+
+                        }else {
+                            return  _response.apiWarning(res,"Low balance!")
+                        }
+
                     }
+
                 }else {
                     return  _response.apiWarning(res,"Low balance!")
                 }
